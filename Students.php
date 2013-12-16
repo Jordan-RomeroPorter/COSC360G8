2ea44f042bbdc94f9ba6be3b6e4f1b44c3aaf39f
Jordan Romero Porter <Jordan.Isidoro@yahoo.com>
2013-12-16 01:53:14 -0800

    students update

--- Students.php
+++ Students.php
@@ -38,12 +38,12 @@
                     $dbminor = mysqli_query($con,"SELECT minor FROM students WHERE rows = $i");
                     $dbexperience = mysqli_query($con,"SELECT experience FROM students WHERE rows = $i");
                     $dbskills = mysqli_query($con,"SELECT skills FROM students WHERE rows = $i");
-                    $dbfblacklist1 = mysqli_query($con,"SELECT blacklist FROM students WHERE rows = $i");
-                    $dbfblacklist2 = mysqli_query($con,"SELECT blacklist FROM students WHERE rows = $i");
-                    $dbfblacklist3 = mysqli_query($con,"SELECT blacklist FROM students WHERE rows = $i");
-                    $dblblacklist1 = mysqli_query($con,"SELECT blacklist FROM students WHERE rows = $i");
-                    $dblblacklist2 = mysqli_query($con,"SELECT blacklist FROM students WHERE rows = $i");
-                    $dblblacklist3 = mysqli_query($con,"SELECT blacklist FROM students WHERE rows = $i");
+                    $dbfblacklist1 = mysqli_query($con,"SELECT fblacklist1 FROM students WHERE rows = $i");
+                    $dbfblacklist2 = mysqli_query($con,"SELECT fblacklist2 FROM students WHERE rows = $i");
+                    $dbfblacklist3 = mysqli_query($con,"SELECT fblacklist3 FROM students WHERE rows = $i");
+                    $dblblacklist1 = mysqli_query($con,"SELECT lblacklist1 FROM students WHERE rows = $i");
+                    $dblblacklist2 = mysqli_query($con,"SELECT lblacklist2 FROM students WHERE rows = $i");
+                    $dblblacklist3 = mysqli_query($con,"SELECT lblacklist3 FROM students WHERE rows = $i");
                     $dbbusydays = mysqli_query($con,"SELECT busydays FROM students WHERE rows = $i");
                     $dbproject = mysqli_query($con,"SELECT project FROM students WHERE rows = $i");
                     $dbteam = mysqli_query($con,"SELECT team FROM students WHERE rows = $i");
